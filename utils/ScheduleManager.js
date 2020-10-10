0764ae55f0dd0b806f2b0601dff7de46c36d6213
akinsola <thomas.lawanson@gmail.com>
2020-10-10 17:39:32 +0100

    update

--- utils/ScheduleManager.js
+++ utils/ScheduleManager.js
@@ -45,7 +45,7 @@ class ScheduleManager {
             if(screenAction.ACTION === "START_AUDIO") {
                 newAction = {
                     ...screenAction, 
-                    TIMECODE: (parseInt(screenAction.TIMECODE)) + 340
+                    TIMECODE: (parseInt(screenAction.TIMECODE)) + 400
                 }
             } 
 
