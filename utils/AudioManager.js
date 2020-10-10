e96509f61b6fa10cd597550943fb6296df0ee16e
akinsola <thomas.lawanson@gmail.com>
2020-10-10 17:13:13 +0100

    update

--- utils/AudioManager.js
+++ utils/AudioManager.js
@@ -34,12 +34,7 @@ class AudioManager {
             } 
 
             this.audio_player = this.manager.create(`./assets/${audio.uri}`, {'-o': 'alsa'});
-            this.audio_player.on('play', function(){ 
-                 
-            })
             this.audio_player.play()
-            let date = new Date()
-            console.log('AFTER CALL AUDIO FUNCTION', date.getMinutes() + ':' + date.getSeconds() + ':' + date.getMilliseconds())
             // this.audio_player = player.play(`./assets/${audio.uri}`), function (err) {
             //     if (err) throw err;
             //     console.log("Audio finished");
